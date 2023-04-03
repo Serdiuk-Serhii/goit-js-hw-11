fe5ff56511e61051573885f3cba79580214095fe
Serdiuk-Serhii <114324858+Serdiuk-Serhii@users.noreply.github.com>
2023-04-03 22:19:15 +0300

    axios

--- src/js/fetchURL.js
+++ src/js/fetchURL.js
@@ -1,9 +1,8 @@
-// import axios from "axios";
+import axios from "axios";
 
 export async function fetchImg(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const response = await axios.get(url);
+    return response.data;
 
     // return fetch(url).then(response => {
     //     if (!response.ok) {
